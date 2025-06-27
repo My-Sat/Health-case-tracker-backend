5dd41cd891a361c08f79843a155cd3399cfe5663
chentiwuni <chienntiwuni@gmail.com>
2025-06-27 16:07:22 +0000

    Updated facility route

--- routes/facilityRoute.js
+++ routes/facilityRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createFacility, getAllFacilities } = require('../Controllers/facilityController');
-const { protect, adminOnly } = require('../middlewares/authMiddleware');
+const { protect, adminOnly } = require('../middleWares/authMiddleware');
 
 router.post('/', protect, adminOnly, createFacility);
 router.get('/', getAllFacilities);
