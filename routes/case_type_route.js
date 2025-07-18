f16cd7588c8efa89f0ee5f58d6affe8c98815f80
chentiwuni <chienntiwuni@gmail.com>
2025-07-18 10:22:59 +0000

    modified case type route

--- routes/case_type_route.js
+++ routes/case_type_route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { create_case_type, list_case_types } = require('../Controllers/case_type_controller');
-const {adminOnly} = require('../middleWares/authMiddleware');
+const { protect, adminOnly } = require('../middleWares/authMiddleware');
 
-router.post('/', adminOnly, create_case_type);
-router.get('/', adminOnly, list_case_types);
+// Protected Admin-Only Routes
+router.post('/', protect, adminOnly, create_case_type);
+router.get('/', protect, adminOnly, list_case_types);
 
 module.exports = router;
