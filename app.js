8746aafa2565e308e167e05d3a6590c07bb5960d
chentiwuni <chienntiwuni@gmail.com>
2025-07-18 21:02:00 +0000

    added case type route in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const userRoutes = require('./routes/user_routes');
 const facilityRoutes = require('./routes/facilityRoute');
 const caseRoutes = require('./routes/caseRoute');
+const caseTypeRoutes = require('./routes/case_type_route');
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/facilities', facilityRoutes);
 app.use('/api/cases', caseRoutes);
+app.use('/api/casetypes', caseTypeRoutes);
+
 
 module.exports = app;
